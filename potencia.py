6cd0e0ebb212db9aafd7b027535db1f9d82b2275
Henry <hyrre9056@gmail.com>
2020-02-27 19:28:48 -0500

    Inicio

---
+++ potencia.py
@@ -0,0 +1,23 @@
+
+import sys
+#sys.argv
+
+entrada = sys.argv
+
+x = entrada[1]
+p = entrada[2]
+vd = entrada[3]
+
+print(x)
+print(p)
+print(vd)
+
+#o=0
+#or i in range(len(x)):
+#	if x[i] != "L":
+#		if x[i] != "=":
+#			if x[i] != "{":
+#				if x[i] != "}":
+#					if x[i] != "l":
+
+#for i in range[cadena]
